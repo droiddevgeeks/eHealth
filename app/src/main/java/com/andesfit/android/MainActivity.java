d930350aca14f05ef936d25a5e28d8f72fdf7364
droiddevgeeks <droiddevgeeks@gmail.com>
2017-05-24 11:11:07 +0530

    Login Changes

--- app/src/main/java/com/andesfit/android/MainActivity.java
+++ app/src/main/java/com/andesfit/android/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_account_layout);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
