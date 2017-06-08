17c3354736d7b74aae5276bb6c99e414a4a3cc38
Anjali <Anjali.punit26@gmail.com>
2017-06-09 00:08:00 +0530

    fragment home

--- app/src/main/java/com/andesfit/android/MainActivity.java
+++ app/src/main/java/com/andesfit/android/MainActivity.java
@@ -9,9 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-import com.andesfit.android.util.HealthSharedPreference;
+import com.andesfit.android.fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -24,20 +23,7 @@ public class MainActivity extends AppCompatActivity
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("My Health");
-        HealthSharedPreference preference = HealthSharedPreference.getInstance(this);
-        ((TextView) findViewById(R.id.name)).setText(preference.getFName() + " " + preference.getLName());
-        ((TextView) findViewById(R.id.weightTv)).setText(preference.getWeight() + "kg");
-        ((TextView) findViewById(R.id.currentWeight)).setText(preference.getWeight() + "kg");
-
-        try {
-            double bmi = Double.valueOf(preference.getWeight()) /
-                    ((Double.valueOf(preference.getHeight()) / 100) *
-                            (Double.valueOf(preference.getHeight()) / 100));
-            bmi = Math.round(bmi);
-            ((TextView) findViewById(R.id.bmiTv)).setText("" + bmi);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        getSupportFragmentManager().beginTransaction().add(R.id.container, new HomeFragment()).commit();
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -98,8 +84,8 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_info) {
-
+        if (id == R.id.nav_my_health) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new HomeFragment()).commit();
         }
         // Handle the camera action
 //        } else if (id == R.id.nav_gallery) {
